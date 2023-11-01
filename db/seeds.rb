8c379305f34afd0af841970b6bd95ada78975152
usausa <143808163+uasuas@users.noreply.github.com>
2023-11-01 05:16:31 +0000

    First commit

---
+++ db/seeds.rb
@@ -0,0 +1,42 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+  name: "testname1",
+  email: "test1@mail",
+  password: "111111"
+)
+User.create!(
+  name: "testname2",
+  email: "test2@mail",
+  password: "111111"
+)
+User.create!(
+  name: "testname3",
+  email: "test3@mail",
+  password: "111111"
+)
+User.create!(
+  name: "testname4",
+  email: "test4@mail",
+  password: "111111"
+)
+User.create!(
+  name: "testname5",
+  email: "test5@mail",
+  password: "111111"
+)
+User.create!(
+  name: "testname6",
+  email: "test6@mail",
+  password: "111111"
+)
+User.create!(
+  name: "testname7",
+  email: "test7@mail",
+  password: "111111"
+)
